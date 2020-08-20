9c67589ddd27d4447d7949ed882dc045bfb11874
lyldelove <932563352@qq.com>
2020-08-21 07:27:41 +0800

    Update 异常机制测试

--- src/main/java/com/lyldelove/matrix/App.java
+++ src/main/java/com/lyldelove/matrix/App.java
@@ -7,5 +7,7 @@ package com.lyldelove.matrix;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        //Class<?> a = Class.forName("a");
+        //new Test().test();
     }
 }
---
+++ src/main/java/com/lyldelove/matrix/Test.java
@@ -0,0 +1,18 @@
+package com.lyldelove.matrix;
+
+/**
+ * @author lyldelove
+ * @title Test
+ * @url
+ * @tag
+ * @date 2020/8/21 5:57
+ */
+public class Test {
+    public void test() throws ClassNotFoundException {
+        try{
+            System.out.println(123);
+        } catch(NullPointerException e) {
+            System.out.println(e);
+        }
+    }
+}
